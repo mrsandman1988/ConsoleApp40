942876efb5c733218cb0efd4b73431aef7352886
sraphunanyan <User@ADMIN>
2023-07-26 19:57:36 +0400

    Add project files.

---
+++ ConsoleApp40/Productcs.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp40
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public int Discount { get; set; }
+        public static bool operator ==(Product first, Product second)
+        {
+            return first.Name == second.Name && first.Price == second.Price;
+        }
+
+        public static bool operator !=(Product first, Product second)
+        {
+            return first.Price != second.Price || first.Name != second.Name;
+        }
+
+        public static bool operator > (Product first, Product second)
+        {
+            return first.Price > second.Price;
+        }
+        public static bool operator <(Product first, Product second)
+        {
+            return first.Price > second.Price;
+        }
+
+        public static Product operator +(Product first, Product second)
+        {
+            return new Product
+            {
+                Id = first.Id,
+                Name = first.Name,
+                Price = first.Price + second.Price,
+                Description = first.Description,
+                Discount = first.Discount > second.Discount ? first.Discount : second.Discount
+            };
+
+        }
+    }
+}
---
+++ ConsoleApp40/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp40
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4 };
+            Product[] products = { new Product { Price = 1000, Discount = 50 } };
+            var r1 = numbers.Min();
+            var r2 = products.Sum(p => p.Price - p.Price/100*p.Discount);
+            var r3 = numbers.Average();
+            var r4 = numbers.All(p=>p %2==0);
+            var r5 = numbers.Count(p=>p%2==0);
+            var r6 = numbers.Max();
+            var r7 = products.MinBy(p => p.Price);
+
+            var r8 = products
+                .Select(p => new { p.Name, Price = p.Price - p.Price / 100 * p.Discount });
+               
+
+            foreach(var item in r8)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            var a = r8.Count();
+
+
+        }
+    }
+}
